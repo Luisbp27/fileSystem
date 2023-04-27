a3e152f1fe9ed490487fd1c6ad40020657046d2c
Bear-03 <64696287+Bear-03@users.noreply.github.com>
2023-04-27 19:53:57 +0200

    Fix level 7

--- src/Level 07/directorios.c
+++ src/Level 07/directorios.c
@@ -10,19 +10,21 @@
  * @return 0 if the path is correct, -1 otherwise
  */
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
-
     if (camino[0] != '/') {
         return FAILURE;
     }
 
-    if (!strchr(camino + 1, '/')) {
-        strcpy(inicial, camino + 1);
-        *tipo = 'f';
-        strcpy(final, "");
+    char *rest = strchr(camino + 1, '/');
+
+    if (rest) {
+        strncpy(inicial, camino + 1, strlen(camino) - strlen(rest) - 1);
+        strcpy(final, rest);
+
+        *tipo = final[0] == '/' ? 'd' : 'f';
     } else {
-        strncpy(inicial, camino + 1, strchr(camino + 1, '/') - camino - 1);
-        *tipo = 'd';
-        strcpy(final, strchr(camino + 1, '/') + 1);
+        strcpy(inicial, camino + 1);
+        strcpy(final, "");
+        *tipo = 'f';
     }
 
     return SUCCESS;
@@ -41,15 +43,17 @@ int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
  * @return 0 if the entry is found, -1 otherwise
  */
 int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsigned int *p_inodo, unsigned int *p_entrada, char reservar, unsigned char permisos) {
+    struct entrada entrada = {
+        .nombre = {0},
+        .ninodo = 0,
+    };
 
-    struct entrada entrada;
+    if (strcmp(camino_parcial, "/") == 0) {
+        super_bloque_t sb;
+        if (bread(POS_SB, &sb) == FAILURE) {
+            return FAILURE;
+        }
 
-    super_bloque_t sb;
-    if (bread(POS_SB, &sb) == FAILURE) {
-        return FAILURE;
-    }
-
-    if (camino_parcial[0] != '/') {
         // Root always be associated with the inode 0
         *p_inodo = sb.posInodoRaiz;
         *p_entrada = 0;
@@ -65,16 +69,26 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
         return ERROR_CAMINO_INCORRECTO;
     }
 
+#if DEBUGIMPORTANT
+    printf("[buscar_entrada()->inicial: %s, final: %s, reservar: %d]\n", inicial,
+           final, reservar);
+#endif
+
     // Search for the entry in the root directory
     inodo_t inodo_dir;
-    leer_inodo(*p_inodo_dir, &inodo_dir);
+    if (leer_inodo(*p_inodo_dir, &inodo_dir) == FAILURE) {
+        return FAILURE;
+    }
+
     if ((inodo_dir.permisos & 4) != 4) {
         return ERROR_PERMISO_LECTURA;
     }
 
     // Initialize the buffer with 0
     struct entrada read_buffer[BLOCKSIZE / sizeof(struct entrada)];
-    memset(read_buffer, 0, (BLOCKSIZE / sizeof(struct entrada)) * sizeof(struct entrada));
+    if (!memset(read_buffer, 0, (BLOCKSIZE / sizeof(struct entrada)) * sizeof(struct entrada))) {
+        return FAILURE;
+    }
 
     // Calculate the number of entries in the inode
     int cant_entradas_inodo = inodo_dir.tamEnBytesLog / sizeof(struct entrada);
@@ -82,23 +96,23 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
 
     // Read the entries in the inode
     if (cant_entradas_inodo > 0) {
-        // Improvement: read the entries in blocks
-        int bytes_leidos = mi_read_f(*p_inodo_dir, read_buffer, 0, BLOCKSIZE);
+        if ((inodo_dir.permisos & 4) != 4) {
+            return ERROR_PERMISO_LECTURA;
+        }
 
-        // Search for the entry
-        while ((num_entrada_inodo < cant_entradas_inodo) && (strcmp(inicial, read_buffer[num_entrada_inodo % (BLOCKSIZE / sizeof(struct entrada))].nombre) != 0)) {
+        if (mi_read_f(*p_inodo_dir, &entrada, num_entrada_inodo * sizeof(struct entrada), sizeof(struct entrada)) == FAILURE) {
+            return FAILURE;
+        }
+
+        while (num_entrada_inodo < cant_entradas_inodo && strcmp(inicial, entrada.nombre) != 0) {
             num_entrada_inodo++;
-
-            // Read the next block
-            if (num_entrada_inodo % (BLOCKSIZE / sizeof(struct entrada)) == 0) {
-                memset(read_buffer, 0, sizeof(read_buffer));
-                bytes_leidos += mi_read_f(*p_inodo_dir, read_buffer, bytes_leidos, BLOCKSIZE);
-            }
+            if (mi_read_f(*p_inodo_dir, &entrada, num_entrada_inodo * sizeof(struct entrada), sizeof(struct entrada)) == FAILURE)
+                return FAILURE;
         }
     }
 
     // If the entry is not found
-    if ((inicial != entrada.nombre) && (num_entrada_inodo = cant_entradas_inodo)) {
+    if (num_entrada_inodo == cant_entradas_inodo && (inicial != read_buffer[num_entrada_inodo % (BLOCKSIZE / sizeof(struct entrada))].nombre)) {
         switch (reservar) {
             case 0:
                 return ERROR_NO_EXISTE_ENTRADA_CONSULTA;
@@ -109,20 +123,23 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
 
                 if ((inodo_dir.permisos & 2) != 2) {
                     return ERROR_PERMISO_ESCRITURA;
-                } else {
-                    strcpy(entrada.nombre, inicial);
-
-                    if (tipo == 'd') {
-                        if (strcmp(inicial, "/") != 0) {
-                            return ERROR_NO_EXISTE_DIRECTORIO_INTERMEDIO;
-                        }
-
-                        entrada.ninodo = reservar_inodo('d', permisos);
-
-                    } else {
-                        entrada.ninodo = reservar_inodo('f', permisos);
-                    }
                 }
+
+                strcpy(entrada.nombre, inicial);
+
+                if (tipo == 'd' && strcmp(final, "/") != 0) {
+                    return ERROR_NO_EXISTE_DIRECTORIO_INTERMEDIO;
+                }
+
+                entrada.ninodo = reservar_inodo(tipo, permisos);
+
+#if DEBUGIMPORTANT
+                printf("[buscar_entrada()->reservado inodo: %d tipo %c con permisos %d para '%s']\n", entrada.ninodo, tipo, permisos, entrada.nombre);
+#endif
+
+#if DEBUGIMPORTANT
+                fprintf(stderr, "[buscar_entrada()->creada entrada: %s, %d] \n", inicial, entrada.ninodo);
+#endif
 
                 // Write the entry in the inode
                 if (mi_write_f(*p_inodo_dir, &entrada, num_entrada_inodo * sizeof(struct entrada), sizeof(struct entrada)) == FAILURE) {
@@ -136,8 +153,8 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
     }
 
     // We arrive at the end of the path
-    if ((strcmp(final, "/")) == 0) {
-        if ((num_entrada_inodo < cant_entradas_inodo) && (reservar = 1)) {
+    if ((strcmp(final, "/")) == 0 || strcmp(final, "") == 0) {
+        if ((num_entrada_inodo < cant_entradas_inodo) && (reservar == 1)) {
             return ERROR_ENTRADA_YA_EXISTENTE;
         }
 
@@ -151,8 +168,6 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
 
         return buscar_entrada(final, p_inodo_dir, p_inodo, p_entrada, reservar, permisos);
     }
-
-    return SUCCESS;
 }
 
 /**
@@ -162,26 +177,29 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
  */
 void mostrar_error_buscar_entrada(int error) {
     switch (error) {
-        case -2:
+        case ERROR_CAMINO_INCORRECTO:
             fprintf(stderr, "Error: Camino incorrecto.\n");
             break;
-        case -3:
+        case ERROR_PERMISO_LECTURA:
             fprintf(stderr, "Error: Permiso denegado de lectura.\n");
             break;
-        case -4:
+        case ERROR_NO_EXISTE_ENTRADA_CONSULTA:
             fprintf(stderr, "Error: No existe el archivo o el directorio.\n");
             break;
-        case -5:
+        case ERROR_NO_EXISTE_DIRECTORIO_INTERMEDIO:
             fprintf(stderr, "Error: No existe algún directorio intermedio.\n");
             break;
-        case -6:
+        case ERROR_PERMISO_ESCRITURA:
             fprintf(stderr, "Error: Permiso denegado de escritura.\n");
             break;
-        case -7:
+        case ERROR_ENTRADA_YA_EXISTENTE:
             fprintf(stderr, "Error: El archivo ya existe.\n");
             break;
-        case -8:
+        case ERROR_NO_SE_PUEDE_CREAR_ENTRADA_EN_UN_FICHERO:
             fprintf(stderr, "Error: No es un directorio.\n");
+            break;
+        default:
+            fprintf(stderr, "Error: Desconocido (código: %d)", error);
             break;
     }
 }
